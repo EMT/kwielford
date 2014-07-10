e44a2cf28437f77d82d05e2c5c2a2f342026fa02
Andy Gott <andy@madebyfieldwork.com>
2014-07-10 14:29:37 +0100

    Add li3 framework.

---
+++ app/extensions/data/Model.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace app\extensions\data;
+
+
+/*
+use lithium\data\Connections;
+Connections::get('default')->applyFilter('_execute', function($self, $params, $chain) {
+    var_dump($params['sql']);
+    return $chain->next($self, $params, $chain);
+});
+*/
+
+
+class Model extends \fieldwork\extensions\data\Model {
+
+
+
+	
+	
+
+}
+
+
+
+
+
+
+?>

10e5aef13ae58f4090f2afbed3e9852fe2b75268
Andy Gott <andy@madebyfieldwork.com>
2014-07-15 16:52:03 +0100

    Bug fix.

--- app/controllers/SlackController.php
+++ app/controllers/SlackController.php
@@ -5,7 +5,7 @@ namespace app\controllers;
 use app\models\Slack;
 use lithium\action\DispatchException;
 
-class SlackController extends \li3_fieldwork\action\Controller {
+class SlackController extends \li3_fieldwork\extensions\action\Controller {
 
 	public function index() {
 		if ($this->request->data) {

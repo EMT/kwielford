daecf4f65ff8099ab6ff7dac6f5e037b1e743173
Andy Gott <andy@madebyfieldwork.com>
2014-07-15 16:50:35 +0100

    Add Slack /slashcommand integration.

---
+++ app/controllers/SlackController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Slack;
+use lithium\action\DispatchException;
+
+class SlackController extends \li3_fieldwork\action\Controller {
+
+	public function index() {
+		if ($this->request->data) {
+			return 'Hello, this is Kwielford. What can I do for you?';
+		}
+	}
+
+}
+
+?>

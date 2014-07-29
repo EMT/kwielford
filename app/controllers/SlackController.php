a3d2a286773862546739263d27fd5c5038a79ed1
Andy Gott <andy@madebyfieldwork.com>
2014-07-29 16:23:32 +0100

    Test.

--- app/controllers/SlackController.php
+++ app/controllers/SlackController.php
@@ -27,7 +27,7 @@ class SlackController extends \li3_fieldwork\extensions\action\Controller {
 				'say' => function($data) {
 					$d = ['content' => '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body>' . $data . '</body></html>'];
 					$response = json_decode($this->_postToUrl('http://printer.exciting.io/print/4h6s3f3s8v7v2h3p', $d));
-					return $response;
+					var_dump($response);
 					if ($response['response'] === 'ok') {
 						return 'Printing… ' . $data;
 					}
